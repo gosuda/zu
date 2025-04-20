b3b5a04ef580e435875c28678b12064d645809a2
lemon-mint <ice.lemons.mint@gmail.com>
2025-04-20 11:47:36 +0900

    Add Coroutine implementation and remove main package

---
+++ coroutine.go
@@ -0,0 +1,27 @@
+package zu
+
+import "iter"
+
+type Coroutine struct {
+	next  func() (struct{}, bool)
+	yield func(struct{}) bool
+}
+
+func (c *Coroutine) Switch() {
+	if c.next != nil {
+		c.next()
+	} else {
+		c.yield(struct{}{})
+	}
+}
+
+func NewCoro(f func(*Coroutine)) *Coroutine {
+	next, _ := iter.Pull(func(yield func(struct{}) bool) {
+		f(&Coroutine{
+			yield: yield,
+		})
+	})
+	return &Coroutine{
+		next: next,
+	}
+}
